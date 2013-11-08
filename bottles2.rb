7f85d366c658c00f9c09e26b6f71f01779c0bee6
Ricky Bacci <brownieelf175@gmail.com>
2013-11-08 15:55:50 -0500

    initial commit

---
+++ bottles2.rb
@@ -0,0 +1,11 @@
+require './english_number2'
+bottle = 99
+
+while bottle > 0
+  bottles = english_number bottle
+  
+  puts bottles + ' bottles of beer on the wall...' + bottles + ' bottles of beer.'
+  puts 'Take one down...pass it around...' + bottles + ' bottles of beer on the wall.'
+  puts
+  bottle -= 1
+end
